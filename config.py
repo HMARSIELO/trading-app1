6ffac8a10164eed3dc428ca19ce709b9a7e3d782
HMARSIELO <hassanmarsielo@gmail.com>
2025-04-05 00:15:55 +0200

    Initial commit

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 # config.py
 
-# إعدادات Binance API
-BINANCE_API_KEY = "shHGonbYkSLoYomekD8w2SUHohcknLEZe60kCykHifu28BhMuVMcZAwvq15UgqUj"
-BINANCE_API_SECRET = "4DyI9k8qBBt2XWpMS63KMsXIHIgXH8Bh8d4B8UXMuuogcZzmQg0ZIeVlMAvCpsnx"
-
 # إعدادات Telegram Bot
 TELEGRAM_BOT_TOKEN = "7661534066:AAHhChkHSHDZxlziFEyhok1vNOlopgbJu1s"
 TELEGRAM_CHAT_ID = "8037372664"
